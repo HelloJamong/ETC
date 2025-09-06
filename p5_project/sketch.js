e6ae5b565620dcab1473eabb5fbc799ee618973d
HelloJamong <hellowook0311@gmail.com>
2025-09-06 19:33:06 +0900

    p5.js 원 여러개 그리기

--- p5_project/sketch.js
+++ p5_project/sketch.js
@@ -1,14 +1,14 @@
 function setup() {
   // 캔버스(스케치북)를 생성합니다.
-  createCanvas(400, 400);
+  createCanvas(800, 600);
 }
 
 function draw() {
   background(220);
 
-  fill(0); // 검은색으로 채우기 설정
-  ellipse(100, 200, 100, 100);
-
-  fill(200); // 밝은 회색으로 채우기 설정
-  ellipse(300, 200, 100, 100);
+  ellipse(300, 300, 500, 500);
+  ellipse(300, 300, 400, 400);
+  ellipse(300, 300, 300, 300);
+  ellipse(300, 300, 200, 200);
+  ellipse(300, 300, 100, 100);
 }
