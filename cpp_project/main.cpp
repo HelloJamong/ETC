16191ad8dd4fc4788af29fab471f7eef3ccd1553
HelloJamong <hellowook0311@gmail.com>
2025-09-13 14:57:24 +0900

    커스텀 헤더 파일 .h 작성 방법 학습

--- cpp_project/main.cpp
+++ cpp_project/main.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
-int add(int a, int b) {
-	return a + b;
-}
-
+using namespace std;
+#include "math_utils.h"
 
 int main(){
-	std::cout << "Hello, World!" << std::endl;
-	std::cout << add(3, 4) << std::endl; 
 
+	std::cout << add(3, 4) << std::endl;
+	/*
+	문자 입력 및 출력
+	std::string txt;
+	std::cin >> txt;
+	std::cout << txt << std::endl;
+	std::cout << "Hello, World!" << std::endl;
+	*/
 	return 0;
 }
-
