483abe51e1cb4ce0f696d8d0c3a7b1866630870a
HelloJamong <hellowook0311@gmail.com>
2025-09-13 15:33:56 +0900

    매크로 사용방법 추가

--- cpp_project/main.cpp
+++ cpp_project/main.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
-using namespace std;
 #include "math_utils.h"
+#define PI 3.14159
+#define SQUARE(x) ((x) * (x))
+using namespace std;
 
 int main(){
+	double r = 5.0;
+	cout << "원의 넓이 : " << PI * SQUARE(r) << endl;
 
-	std::cout << add(3, 4) << std::endl;
+
 	/*
+	헤더를 사용한 함수 호출
+	std::cout << add(3, 4) << std::endl;
+	
 	문자 입력 및 출력
 	std::string txt;
 	std::cin >> txt;
