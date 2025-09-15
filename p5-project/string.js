dac0f78ac3bae508dea4ed733fc0da45904e4898
HelloJamong <hellowook0311@gmail.com>
2025-09-15 20:38:37 +0900

    문자열 공부

---
+++ p5-project/string.js
@@ -0,0 +1,31 @@
+let str1 = "Hello, ";
+let str2 = "World! ";
+let str3 = "JavaScript";
+let str4 = str1 + str2;
+let str5 = "   Hello World!   ";
+let subject = "프로그래밍";
+
+console.log(str3[0]);
+console.log(str3[4])
+
+console.log(subject);
+
+console.log(subject[0]);
+console.log(subject[4]);
+
+console.log(str3.length);
+
+console.log(str1 + str2);
+console.log(str1.concat(str2));
+
+console.log(str4.slice(0, 5));
+console.log(str4.slice(-6, -1));
+console.log(str4.slice(7));
+
+console.log(str4.indexOf("World"));
+console.log(str4.includes("Hello"));
+
+console.log(str3.toUpperCase());
+console.log(str3.toLowerCase());
+
+console.log(str5.trim());
