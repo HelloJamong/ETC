030ec60bf76df4c1d300cb23d8dc6c0f23208d7b
HelloJamong <hellowook0311@gmail.com>
2025-10-20 19:25:16 +0900

    edit

--- cpp_project/main.cpp
+++ cpp_project/main.cpp
@@ -1,14 +1,35 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void show_message(string msg = "Hi") {
-    cout << msg << endl;
+
+void increment(int &x){
+	x += 1;
+}
+
+void printVector(const vector<int>& v){
+	for(int x : v) cout << x << " ";
+	cout << endl;
+
+}
+
+void divide(int a, int b, int &quotient, int &remainder){
+	quotient = a / b;
+	remainder = a % b;
 }
 
 int main(){
-	show_message();       // 출력: Hi
-	show_message("Bye");  // 출력: Bye
-	
+	int value = 10;
+	increment(value);
+	cout << value << endl;
+
+	vector<int> data = {1, 2, 3, 4, 5};
+	printVector(data);
+
+	int q, r;
+	divide(17, 5, q, r);
+	cout << "몫 : " << q << ", 나머지 : " << r << endl;
+
 	return 0;
 }
 
