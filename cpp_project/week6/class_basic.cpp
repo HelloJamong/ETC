d20ecee206ef88452568e5dfa28552a5a5a0141c
HelloJamong <hellowook0311@gmail.com>
2025-11-01 14:38:38 +0900

    edit src

--- cpp_project/week6/class_basic.cpp
+++ cpp_project/week6/class_basic.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-
+//포인터 선언과 사용
+/*
 int main() {
     int a = 5;
     int* p = &a;
@@ -11,5 +12,18 @@ int main() {
     *p = 20;  // 포인터를 통해 a의 값을 변경
     cout << "a (after *p = 20): " << a << endl;
 
+    return 0;
+}
+*/
+
+//포인터와 함수(함수 인자 전달 방식)
+void change(int x){
+    x = 100;
+}
+
+int main(){
+    int a= 5;
+    change(a);
+    cout << a << endl;
     return 0;
 }
